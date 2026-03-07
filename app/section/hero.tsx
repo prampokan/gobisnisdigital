5a387d8397b16b2a14d7d743bc5d06c9f78f88e8
prampokan@gmail.com <prampokan@gmail.com>
2026-03-08 00:50:32 +0700

    feat: katah

--- app/section/hero.tsx
+++ app/section/hero.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
-import { MoveRight } from "lucide-react";
+import { MoveRight, Rocket } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <div className="w-full flex flex-col items-center text-center">
+    <div className="w-full flex flex-col items-center text-center py-20">
       <div className="flex items-center gap-x-4">
-        {/* <div className="w-20 h-20">
+        <div className="w-20 h-20">
           <Image
-            src="/assets/images/logo2.png"
+            src="/assets/images/logo3.png"
             alt="logo"
             width={200}
             height={200}
             className="w-full h-full onject-cover object-center"
           />
-        </div> */}
+        </div>
         <h1 className="text-4xl sm:text-6xl md:text-7xl font-semibold tracking-tighter text-accent-foreground">
-          ⚡Digitalin Bisnismu!
+          Digitalin Bisnismu!
         </h1>
       </div>
       <h2 className="text-xl sm:text-2xl font-medium tracking-tight text-accent-foreground mt-5 sm:mt-12 max-w-md leading-relaxed">
